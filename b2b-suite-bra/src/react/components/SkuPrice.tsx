8c60298710c468c10c49acd8c235f8a561dd5bb5
wellington.pedro <wellington.pedro@cadastra.com>
2025-01-12 14:21:04 -0300

    feat: add simulation request

--- b2b-suite-bra/src/react/components/SkuPrice.tsx
+++ b2b-suite-bra/src/react/components/SkuPrice.tsx
@@ -1,31 +1,78 @@
-import React from 'react'
-import { useSku } from './SkuContext'
-import { Item } from '../typings'
-import { useCssHandles } from 'vtex.css-handles'
-import { path } from 'ramda'
-import { FormattedCurrency } from 'vtex.format-currency'
-import { FormattedMessage } from 'react-intl'
+import { path } from "ramda";
+import { useEffect, useState } from "react";
+import { FormattedMessage } from "react-intl";
+import { useCssHandles } from "vtex.css-handles";
+import { FormattedCurrency } from "vtex.format-currency";
+import { Item } from "../typings";
+import { useProviderWhiteLabel } from "./ProviderWhiteLabel/ProviderWhiteLabel";
+import { useSku } from "./SkuContext";
 
-const CSS_HANDLES = ['priceContainer'] as const
+const CSS_HANDLES = ["priceContainer"] as const;
 
 interface Props {
-  showLabel: boolean
+  showLabel: boolean;
 }
 
 const SkuPrice = ({ showLabel }: Props) => {
-  const { sku }: { sku: Item } = useSku()
-  const sellingPrice: number | undefined = path(
-    ['sellers', 0, 'commertialOffer', 'Price'],
+  const { sku }: { sku: Item } = useSku();
+  const sellingPriceDefault: number | undefined = path(
+    ["sellers", 0, "commertialOffer", "Price"],
     sku
-  )
-  const handles = useCssHandles(CSS_HANDLES)
+  );
+  const [sellingPrice, setSellingPrice] = useState<number | undefined>(
+    sellingPriceDefault
+  );
+  const handles = useCssHandles(CSS_HANDLES);
+  const provider = useProviderWhiteLabel();
+  const sellerId = provider.seller;
+  useEffect(() => {
+    const fetchSimulation = async () => {
+      try {
+        if (sellerId !== "1") {
+          const response = await fetch(
+            "/api/checkout/pub/orderForms/simulation",
+            {
+              method: "POST",
+              headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({
+                items: [
+                  {
+                    id: sku?.itemId,
+                    quantity: 1,
+                    seller: sellerId,
+                  },
+                ],
+                country: "BRA",
+              }),
+            }
+          );
+
+          if (!response.ok) {
+            throw new Error(`Error: ${response.status}`);
+          }
+
+          const data = await response.json();
+          const price = data.items[0].price;
+          setSellingPrice(price / 100);
+        }
+      } catch (error) {
+        console.error("Failed to fetch simulation", error);
+      }
+    };
+
+    fetchSimulation();
+  }, [sku, provider.seller]);
 
   return sellingPrice ? (
     <div
-      className={`pt3 pb5 t-body c-muted-1 lh-copy ${handles.priceContainer}`}>
+      className={`pt3 pb5 t-body c-muted-1 lh-copy ${handles.priceContainer}`}
+    >
       {showLabel && (
         <span className="t-body c-on-base fw7 pr3">
-          <FormattedMessage id="store/sku-list.sku.price.title" />:{' '}
+          <FormattedMessage id="store/sku-list.sku.price.title" />:{" "}
         </span>
       )}
       <span>
@@ -34,7 +81,7 @@ const SkuPrice = ({ showLabel }: Props) => {
     </div>
   ) : (
     <div />
-  )
-}
+  );
+};
 
-export default SkuPrice
+export default SkuPrice;
